fc833fec6c68894577dbb12098445d01ae33cae1
Philip Schultz <24783715+schupp13@users.noreply.github.com>
2019-04-12 03:51:36 -0400

    Update planets.php

--- planets/planets.php
+++ planets/planets.php
@@ -1,7 +1,7 @@
     <?php
     $servername = "localhost";
-    $username = "schupp";
-    $password = "W53ZoOVSXmQk";
+    $username = "";
+    $password = "";
     $dbname = "planets";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
